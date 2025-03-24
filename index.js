093f35927028bf363875dd9af4f62020a8079189
amirmejri96 <amirmejri1996@gmail.com>
2025-03-24 16:09:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const port = 4000
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const multer = require('multer')
+const path = require('path')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+
+// DataBase connection with mongoDB
+mongoose.connect("mongodb+srv://amirmejri1996:Habibradhia2308@cluster0.b8n12.mongodb.net/e-commerce")
+
+// API creation 
+app.get('/', (req, res) => {
+    res.send('Express app is running')
+})
+
+// Image storage engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({storage:storage})
+
+// Creating upload endpoint for images
+app.use('/images', express.static('upload/images'))
+
+app.post('/upload', upload.single('product'),(req, res)=>{
+res.json({
+    success:1,
+    image_url:`http://localhost:${port}/images/${req.file.filename}`
+})
+})
+
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server running on port" + port)
+    } else {
+        console.log("Error : " + error)
+    }
+})
